5c4bc04abf81522b6bccc2621c68d8aad35c993b
Raphael Tony <prince4raphael@gmail.com>
2021-05-05 23:53:23 +0530

    Added Comments

--- quickstart.py
+++ quickstart.py
@@ -16,6 +16,9 @@ SCOPES = ['https://www.googleapis.com/auth/documents.readonly']
 DOCUMENT_ID = ''
 
 
+# This function takes the text in a paragraph of the document and splits it up in a way such that
+# it ignores punctuations (done by the 're' command) and stores all the words in a list(res).
+# Then it checks if the word 'attendance' exists in the list
 def checkAttendance(sent):
     res = re.findall(r'\w+', sent)
     for i in res:
@@ -27,8 +30,8 @@ def checkAttendance(sent):
 def main():
     print("Enter the document ID : ")
     DOCUMENT_ID = input()
-    """Shows basic usage of the Docs API.
-    Prints the title of a sample document.
+
+    """Shows basic usage of the Docs API. This code is a snippet from the Docs API website:
     """
     creds = None
     # The file token.json stores the user's access and refresh tokens, and is
@@ -50,12 +53,14 @@ def main():
 
     service = build('docs', 'v1', credentials=creds)
 
-    # Retrieve the documents contents from the Docs service.
     currentIndex = 1
     notFound = True
     while notFound:
+        # Retrieve the documents contents from the Docs service. Here document is a dictionary
+        # that has a lot of nested dictionaries and lists
         document = service.documents().get(documentId=DOCUMENT_ID).execute()
 
+        # Going through every paragraph in the document
         for i in range(currentIndex, len(document['body']['content'])):
             print("i =  :", i)
             try:
@@ -64,7 +69,16 @@ def main():
                     notFound = False
                     break
                 else:
+                    # This is so that the next time the document is retrieved, it doesn't have to
+                    # go through all the paragraphs again. It just begins searching for the word
+                    # at the latest paragraph.
                     currentIndex = i
+
+                    # In case, there are chat messages during the Gmeet, the document gets
+                    # modified by the chrome extension. In that case, we have to manually stop
+                    # the search whenever we find this page seperator. The reason its i+3 is
+                    # because after the text of the latest paragraph, there are two '\n'
+                    # characters, only after which the seperator occurs in the document
                     if document['body']['content'][i+3]['paragraph']['elements'][0]['textRun']['content'] == "____________________________________________________________________________\n":
                         break
 
@@ -73,11 +87,11 @@ def main():
             except KeyError:
                 continue
 
-        time.sleep(2)
+        time.sleep(2)  # Checking every 2 seconds
 
     print("Alert!")
     telegram_send.send(messages=["attendance"])
-    playsound('run.mp3')
+    # playsound('run.mp3')
 
 
 if __name__ == '__main__':
