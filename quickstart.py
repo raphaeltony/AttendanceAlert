15eb258c8d5c2728e814470b76ec563136b2bc80
Raphael Tony <prince4raphael@gmail.com>
2021-05-08 20:50:33 +0530

    Added Drive API to automatically access latest doc file

--- quickstart.py
+++ quickstart.py
@@ -10,10 +10,13 @@ from google.auth.transport.requests import Request
 from google.oauth2.credentials import Credentials
 
 # If modifying these scopes, delete the file token.json.
-SCOPES = ['https://www.googleapis.com/auth/documents.readonly']
+SCOPES = ['https://www.googleapis.com/auth/documents.readonly',
+          'https://www.googleapis.com/auth/drive.metadata.readonly']
 
 # The ID of a sample document.
 DOCUMENT_ID = ''
+folderID = ''
+latestItem = ''
 
 
 def checkAttendance(sent):
@@ -26,7 +29,7 @@ def checkAttendance(sent):
 
 def main():
     print("Enter the document ID : ")
-    DOCUMENT_ID = input()
+    #DOCUMENT_ID = input()
     """Shows basic usage of the Docs API.
     Prints the title of a sample document.
     """
@@ -48,36 +51,63 @@ def main():
         with open('token.json', 'w') as token:
             token.write(creds.to_json())
 
-    service = build('docs', 'v1', credentials=creds)
+    service = build('drive', 'v3', credentials=creds)
+
+    results = service.files().list(
+        q="mimeType = 'application/vnd.google-apps.folder'", pageSize=10, fields="nextPageToken, files(id, name)").execute()
+    items = results.get('files', [])
+
+    if not items:
+        print('No files found.')
+    else:
+        print('Files:')
+        for item in items:
+            if item['name'] == "Meet Transcript":
+                folderID = item['id']
+            print(u'{0} ({1})'.format(item['name'], item['id']))
+    print(folderID)
+
+    results = service.files().list(
+        q="parents in '{}'".format(folderID), pageSize=1, fields="nextPageToken, files(id, name)").execute()
+    items = results.get('files', [])
+
+    if not items:
+        print('No files found.')
+    else:
+        print('Files:')
+        for item in items:
+            print(u'{0} ({1})'.format(item['name'], item['id']))
+        latestItem = items[0]['id']
+        print(latestItem)
 
     # Retrieve the documents contents from the Docs service.
-    currentIndex = 1
-    notFound = True
-    while notFound:
-        document = service.documents().get(documentId=DOCUMENT_ID).execute()
+    # currentIndex = 1
+    # notFound = True
+    # while notFound:
+    #     document = service.documents().get(documentId=DOCUMENT_ID).execute()
 
-        for i in range(currentIndex, len(document['body']['content'])):
-            print("i =  :", i)
-            try:
-                if checkAttendance(document['body']['content'][i]['paragraph']
-                                   ['elements'][1]['textRun']['content']):
-                    notFound = False
-                    break
-                else:
-                    currentIndex = i
-                    if document['body']['content'][i+3]['paragraph']['elements'][0]['textRun']['content'] == "____________________________________________________________________________\n":
-                        break
+    #     for i in range(currentIndex, len(document['body']['content'])):
+    #         print("i =  :", i)
+    #         try:
+    #             if checkAttendance(document['body']['content'][i]['paragraph']
+    #                                ['elements'][1]['textRun']['content']):
+    #                 notFound = False
+    #                 break
+    #             else:
+    #                 currentIndex = i
+    #                 if document['body']['content'][i+3]['paragraph']['elements'][0]['textRun']['content'] == "____________________________________________________________________________\n":
+    #                     break
 
-            except IndexError:
-                continue
-            except KeyError:
-                continue
+    #         except IndexError:
+    #             continue
+    #         except KeyError:
+    #             continue
 
-        time.sleep(2)
+    #     time.sleep(2)
 
-    print("Alert!")
-    telegram_send.send(messages=["attendance"])
-    playsound('run.mp3')
+    # print("Alert!")
+    # telegram_send.send(messages=["attendance"])
+    # playsound('run.mp3')
 
 
 if __name__ == '__main__':
